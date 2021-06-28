8ae7e18deeaba1e03dca6333805ade0788c22069
lephuocduy <duylp.work@gmail.com>
2021-06-29 00:23:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapStateMap from './BaiTapStateMap/BaiTapStateMap';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapStateMap />
+    </div>
+  );
+}
+
+export default App;
